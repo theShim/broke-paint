b8c4525b7595cfdd84cb95b00a1709f356bc292b
theShim <116192010+theShim@users.noreply.github.com>
2024-05-22 14:15:06 +0100

    Update core_funcs.py

--- scripts/core_funcs.py
+++ scripts/core_funcs.py
@@ -26,3 +26,14 @@ def rotate(origin, point, angle):
     qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
     qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
     return vec(qx, qy)
+
+    ##############################################################################################
+
+def roundline(srf, color, start, end, radius=1, width=0):
+    dx = end[0]-start[0]
+    dy = end[1]-start[1]
+    distance = max(abs(dx), abs(dy))
+    for i in range(int(distance)):
+        x = int( start[0]+float(i)/distance*dx)
+        y = int( start[1]+float(i)/distance*dy)
+        pygame.draw.circle(srf, color, (x, y), radius, width)
